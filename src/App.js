60f2332cbbded9309def126423cbb8b120a4c1cb
Olena Vaskevych <olenaj21@gmail.com>
2021-01-08 14:33:10 +0100

    Inner circle

--- src/App.js
+++ src/App.js
@@ -14,7 +14,20 @@ function App() {
           <circle className="sp-pie-slice-2" cx="50%" cy="50%" r="25%" />
           <circle className="sp-pie-slice-3" cx="50%" cy="50%" r="25%" />
         </svg>
-        <div className="smallerCircle3"></div>
+
+      </div>
+      <div class="wrap">
+        <div class="wrap-in">
+          <div class="rw">
+            <div class="inline push green" id='3' ></div>
+
+          </div>
+          <div class="rw">
+            <div class="b-l inline push yellow" id='1'></div>
+            <div class="b-r inline push blue" id='0'></div>
+          </div>
+        </div>
+        <div class="center"></div>
       </div>
 
     </>
